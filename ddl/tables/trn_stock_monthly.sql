cc73bc4889f4fd1468726fcc014ba32e25b6a1df
Gang Wei <mr.wgang@gmail.com>
2016-01-06 23:53:29 +0900

    Add stock weekly & monthly tables

---
+++ ddl/tables/trn_stock_monthly.sql
@@ -0,0 +1,14 @@
+CREATE TABLE trn_stock_monthly
+(
+  code integer NOT NULL,
+  regist_date date NOT NULL,
+  opening_price integer NOT NULL,
+  high_price integer NOT NULL,
+  low_price integer NOT NULL,
+  finish_price integer NOT NULL,
+  turnover integer,
+  CONSTRAINT stock_monthly_pkey PRIMARY KEY (code , regist_date ),
+  CONSTRAINT instrument_fkey FOREIGN KEY (code)
+      REFERENCES mst_instrument (code) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
