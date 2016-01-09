21e8db5d1581da415ea50155703bd9804f565b44
Gang Wei <mr.wgang@gmail.com>
2016-01-09 21:02:32 +0900

    Add foreign key on code to trn_loaded_log

--- ddl/tables/trn_loaded_log.sql
+++ ddl/tables/trn_loaded_log.sql
@@ -5,5 +5,8 @@
 CREATE TABLE trn_loaded_log
 (
   code integer NOT NULL,
-  CONSTRAINT loaded_pkey PRIMARY KEY (code )
+  CONSTRAINT loaded_pkey PRIMARY KEY (code ),
+  CONSTRAINT instrument_fkey FOREIGN KEY (code)
+      REFERENCES mst_instrument (code) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 );
